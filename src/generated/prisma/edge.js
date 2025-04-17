f19197659a12dfb411f32263dc5b7a892e474d53
2005LIKHITH <likhithcvsrl@gmail.com>
2025-04-17 22:26:32 +0530

    FIXED Package

--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -187,12 +187,11 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": true,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": null
+        "value": "postgresql://neondb_owner:npg_5n6AtYfDvNUz@ep-dark-mouse-a19ub4i7-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
       }
     }
   },
